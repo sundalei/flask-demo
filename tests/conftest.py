bc4b6ae304509528ac5f3c700371e219cbd494d5
Leo Sun <leosun@Leos-MacBook-Pro.local>
2020-04-02 19:09:11 +0800

    test coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,57 @@ from flaskr.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
-    print(_data_sql)
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+    print('db_fd', db_fd, 'db_path', db_path)
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    print('app start.')
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+    print('app ended.')
+
+
+@pytest.fixture
+def client(app):
+    print('client fixture.')
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+
+class AuthAction(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthAction(client)
